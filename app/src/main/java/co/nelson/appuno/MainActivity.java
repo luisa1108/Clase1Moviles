f9a475b876aa006206e23dee51a75f9f24c9626a
luisa1108 <159147642+luisa1108@users.noreply.github.com>
2024-02-13 09:47:33 -0500

    Clase 3
    Se implementaron los ciclos de vida
    OnCreate, OnStart, OnResume, OnPause, OnStop, OnDestroy

--- app/src/main/java/co/nelson/appuno/MainActivity.java
+++ app/src/main/java/co/nelson/appuno/MainActivity.java
@@ -3,6 +3,7 @@ package co.nelson.appuno;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,12 +12,24 @@ public class MainActivity extends AppCompatActivity {
 
    TextView nombreUsuario;
    Button btn1,btn2;
+   String Tag = "Prueba";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    }
+
+    /*public double calcularArea(int l1,int l2){
+        return l1*l2;
+    }*/
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(Tag, "Estoy en onStart");
         nombreUsuario = (TextView) findViewById(R.id.usuario);
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
@@ -36,12 +49,40 @@ public class MainActivity extends AppCompatActivity {
                 nombreUsuario.setText("Presioné Boton Dos");
             }
         });
-
-
     }
 
-    public double calcularArea(int l1,int l2){
-        return l1*l2;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(Tag, "Estoy en onResume");
+        nombreUsuario.setText(nombreUsuario.getText().toString() + "Estoy en onResume");
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(Tag, "Estoy en onPause");
+        nombreUsuario.setText(nombreUsuario.getText().toString() + "Estoy en onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(Tag, "Estoy en onStop");
+        nombreUsuario.setText(nombreUsuario.getText().toString() + "Estoy en onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(Tag, "Estoy en onRestart");
+        nombreUsuario.setText(nombreUsuario.getText().toString() + "Estoy en onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(Tag, "Estoy en onDestroy");
+        nombreUsuario.setText(nombreUsuario.getText().toString() + "Estoy en onDestroy");
+    }
 }
